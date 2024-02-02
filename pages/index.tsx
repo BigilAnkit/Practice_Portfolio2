f53b4b5fa1065aefa8331c494c935d34e49c7a1d
BigilAnkit <gehlotankit19@gmail.com>
2024-02-02 09:20:47 +0530

    protfolio using react tailwind typescript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Nav from '@/components/Nav'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='overflow-hidden'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default HomePage
